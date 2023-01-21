6914cd12106cce643dc747946de912572069a2f5
TkNazarii <tknazkooo@gmail.com>
2023-01-21 11:35:28 +0200

    4

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,8 @@
+//стилі логів спливаючих з бібліотеки
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 Notify.init({ cssAnimationStyle: 'from-top', fontAwesomeIconStyle: 'shadow' });
 
+// обєкт посилань
 const refs = {
   form: document.querySelector('.form'),
   firstDelay:document.querySelector('input[name = "delay"]'),
@@ -8,20 +10,26 @@ const refs = {
   amount:document.querySelector('input[name = "amount"]'),
 };
 
+// слухач відправлення форми
 refs.form.addEventListener('submit', onFormSubmit);
 
+// функція відправлення форми
 function onFormSubmit(evt) {
+	// обнуляємо поведімку браузера
   evt.preventDefault();
+//   значення полів
   const firstDelay = Number(refs.firstDelay.value);
   const step = Number(refs.step.value);
   const amount = Number(refs.amount.value);
   
-  for (let i = 1; i <= amount; i += 1){
+//   скільки разів виконати проміс
+  for (let i = 1; i <= amount; i += 1) {
     const delayStep = firstDelay + step * (i - 1);
     createPromise(i, delayStep).then(onSuccess).catch(onError)
   };
 };
 
+// створюємо проміс
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
